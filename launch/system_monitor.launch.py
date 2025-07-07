8909d3fefe5dceab45ed729aede6fc8ac2142d9c
nino192 <nino.dragicevic@probotica.hr>
2025-07-07 15:05:36 +0200

    change defaults

--- launch/system_monitor.launch.py
+++ launch/system_monitor.launch.py
@@ -56,9 +56,9 @@ def generate_launch_description():
     declare_enable_mem_monitor = DeclareLaunchArgument(
         name="enable_mem_monitor", default_value="true", description="enable mem_monitor if true")
     declare_enable_net_monitor = DeclareLaunchArgument(
-        name="enable_net_monitor", default_value="true", description="enable net_monitor if true")
+        name="enable_net_monitor", default_value="false", description="enable net_monitor if true")
     declare_enable_ntp_monitor = DeclareLaunchArgument(
-        name="enable_ntp_monitor", default_value="true", description="enable ntp_monitor if true")
+        name="enable_ntp_monitor", default_value="false", description="enable ntp_monitor if true")
 
     cpu_monitor = Node(
         package="ros2_system_monitor",
